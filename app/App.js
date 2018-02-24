08d96989f159ff17f1120a51a4b62acf6b28f156
Faiz Shahudin <faizshahudin@gmail.com>
2018-02-25 06:22:37 +0800

    first commit

---
+++ app/App.js
@@ -0,0 +1,49 @@
+const React = require('react');
+const ReactRouter = require('react-router-dom');
+require('./index.css');
+
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
+const PersonalDetail = require('./components/PersonalDetail');
+const FamilyDetail = require('./components/FamilyDetail');
+const Nav = require('./components/Nav');
+
+class App extends React.Component {
+    render() {
+       return (
+           <Router>
+               <div className="containerStyle">
+                   <div className="formWrapperStyle">
+                       <div className="headerStyle">
+                           <h3 style={styles.headerTextStyle}>Application Form</h3>
+                       </div>
+                       <Switch>
+                           <Route exact path="/" component={PersonalDetail} />
+                           <Route exact path="/family" component={FamilyDetail} />
+                           <Route render={() => {
+                                return <p>Error 404: Not Found</p>;
+                            }} />
+                       </Switch>
+                       <div className="progressSectionStyle">
+                           Progress Bar Section
+                       </div>
+                       <div className="navSectionStyle">
+                        <Nav />
+                       </div>
+                   </div>
+               </div>
+           </Router>
+       );
+   }
+}
+
+const styles = {
+   headerTextStyle: {
+       fontFamily: 'Open Sans',
+       fontWeight: 'regular',
+       color: 'white',
+   },
+};
+
+module.exports = App;
---
+++ app/components/PersonalDetail.js
@@ -0,0 +1,149 @@
+const React = require('react');
+
+class PersonalDetail extends React.Component {
+    render() {
+        return (
+            <form
+                className="formStyle"
+                onSubmit={this.handleSubmit}
+            >
+                <div className="name">
+                    <label style={styles.labelStyle}>
+                        Full Name (As per IC)
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="ic">
+                    <label style={styles.labelStyle}>
+                        IC Number
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="nat">
+                    <label style={styles.labelStyle}>
+                        Nationality
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="dob">
+                    <label style={styles.labelStyle}>
+                        Date of Birth
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="gender">
+                    <label style={styles.labelStyle}>
+                        Gender
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="address">
+                    <label style={styles.labelStyle}>
+                        Permanent Address
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="postcode">
+                    <label style={styles.labelStyle}>
+                        Postcode
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="state">
+                    <label style={styles.labelStyle}>
+                        State
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="phone">
+                    <label style={styles.labelStyle}>
+                        Phone Number
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="email">
+                    <label style={styles.labelStyle}>
+                        Email Address
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="work">
+                    <label style={styles.labelStyle}>
+                        Are you currently working?
+                    </label>
+                    <input
+                        style={styles.inputStyle}
+                        placeholder=" Type something here"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                </div>
+            </form>
+        );
+    }
+}
+
+const styles = {
+    labelStyle: {
+        fontWeight: 'regular',
+        paddingBottom: 15,
+        fontFamily: 'Open Sans',
+    },
+    inputStyle: {
+        borderRadius: 7,
+        marginBottom: 30,
+        height: 25,
+    },
+};
+
+module.exports = PersonalDetail;
